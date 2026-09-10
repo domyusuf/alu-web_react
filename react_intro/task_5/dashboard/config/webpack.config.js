3f0567c85d03c1cd48b5543f8e5efc66ba6e31b6
Dominion Yusuf <d.yusuf@alustudent.com>
2026-09-10 18:52:33 +0200

    feat: configure Babel for React with webpack in task_5

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -12,8 +12,23 @@ module.exports = {
     static: path.resolve(__dirname, '../dist'),
     hot: true,
   },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   module: {
     rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        ],
+      },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
