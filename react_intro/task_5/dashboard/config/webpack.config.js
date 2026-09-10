d42fe0158754bf0fbcde8677b49bf421ef1c25fe
Dominion Yusuf <d.yusuf@alustudent.com>
2026-09-10 18:50:07 +0200

    feat: initialize custom Webpack setup without create-react-app for task_5

---
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.resolve(__dirname, '../dist'),
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
